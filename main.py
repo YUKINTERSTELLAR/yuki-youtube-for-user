a6025ac3e8de9349fecf11d564b5d1769958de18
雪見だいふく <100012108+mochidukiyukimi@users.noreply.github.com>
2023-01-01 18:46:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-req = __import__("requests")
+import requests
 async def app(scope,receive,send):
-    res = req.get("https://58yr01.deta.dev"+scope["path"]+"?"+scope["query_string"].decode(),cookies={i[0]:i[1] for i in [i.split("=") for i in [i[1] for i in scope["headers"] if i[0] == b"cookie"][0].decode().split(";")]})
+    res = requests.get("https://58yr01.deta.dev"+scope["path"]+"?"+scope["query_string"].decode(),cookies={i[0]:i[1] for i in [i.split("=") for i in [i[1] for i in scope["headers"] if i[0] == b"cookie"][0].decode().split(";")]})
     await send({'type': 'http.response.start','status': res.status_code,'headers': [(b'content-type', res.headers['content-type'].encode()),(b'content-length', str(len(res.content)).encode()),]})
     await send({'type': 'http.response.body','body': res.content})
