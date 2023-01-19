4d6a53072632a7e2a416d5e1b33c1c8f3c9d4051
雪見だいふく <100012108+mochidukiyukimi@users.noreply.github.com>
2023-01-19 21:31:40 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import requests
+url = requests.get(r'https://raw.githubusercontent.com/mochidukiyukimi/yuki-youtube-instance/main/instance.txt')
 async def app(scope,receive,send):
+    global url
     if (cookies := [i[1] for i in scope["headers"] if i[0] == b"cookie"]) and "yuki=True" in cookies[0].decode():
-        res = requests.get("https://58yr01.deta.dev"+scope["path"]+"?"+scope["query_string"].decode(),cookies={i[0]:i[1] for i in [i.split("=") for i in [i[1] for i in scope["headers"] if i[0] == b"cookie"][0].decode().split(";")]})
+        res = requests.get("url"+scope["path"]+"?"+scope["query_string"].decode(),cookies={i[0]:i[1] for i in [i.split("=") for i in [i[1] for i in scope["headers"] if i[0] == b"cookie"][0].decode().split(";")]})
     else:
-        res = requests.get("https://58yr01.deta.dev"+scope["path"]+"?"+scope["query_string"].decode())
+        res = requests.get("url"+scope["path"]+"?"+scope["query_string"].decode())
     await send({'type': 'http.response.start','status': res.status_code,'headers': [(i[0].encode(),i[1].encode()) for i in res.headers.items()]})
     await send({'type': 'http.response.body','body': res.content})
